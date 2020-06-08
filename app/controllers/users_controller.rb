5dc78fd99cfe746ee0d556d386c4f1138981a74e
remon2929 <ff.remon.9123@gmail.com>
2020-06-08 15:48:38 +0900

    13.52

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -50,7 +51,7 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-
+ 
   
   private
 
@@ -61,14 +62,6 @@ class UsersController < ApplicationController
 
   # beforeアクション
 
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   # 正しいユーザーかどうか確認
   def correct_user
